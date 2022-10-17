e99490361fccd7221e1c577b53c5d63cf9792b0a
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-10-17 19:22:23 +0530

    feat: Toggle password

---
+++ app/src/js/index.js
@@ -0,0 +1,13 @@
+const passwordInput = document.querySelector('#password-input');
+const togglePasswordBtn = document.querySelector('#toggle-password');
+togglePasswordBtn.addEventListener('click', togglePassword)
+
+function togglePassword() {
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        togglePasswordBtn.innerText = 'Hide';
+    } else {
+        passwordInput.type = 'password';
+        togglePasswordBtn.innerText = 'Show';
+    }
+}
