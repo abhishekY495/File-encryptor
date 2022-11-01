c4ad07863bb940e2ff81cf25b06828fbd9d5cb80
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-01 21:10:23 +0530

    Update preload.js

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -1,14 +1,4 @@
 const { contextBridge } = require('electron');
-const fs = require('fs');
-const crypto = require('crypto');
-const stream = require('stream');
+const { file } = require('../backend/fileEncDec.js');
 
-contextBridge.exposeInMainWorld('fs', {
-    readFileSync: (...args) => fs.readFileSync(...args),
-    createReadStream: () => fs.createReadStream(),
-    createWriteStream: () => fs.createWriteStream()
-})
-
-contextBridge.exposeInMainWorld('stream', {
-    Transform: (...args) => stream.Transform(...args)
-})
+contextBridge.exposeInMainWorld('file', file);
