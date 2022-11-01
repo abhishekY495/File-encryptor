1866790d7f85304f994f9db271aef3f05577a9df
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-01 21:10:13 +0530

    Retrieving full file path

--- app/src/js/browseFile.js
+++ app/src/js/browseFile.js
@@ -10,11 +10,12 @@ browseFoldersBtn.addEventListener('change', (e) => getFilesFromFolder(e));
 function getFiles(e) {
     const files = e.target.files;
     for (let file in files) {
-        const filePath = files[file].name;
-        if (filePath) {
+        const fileName = files[file].name;
+        const filePath = files[file].path;
+        if (fileName) {
             const element = `
                 <li class="list-none w-full px-2 py-px text-start border-b-2 border-neutral-700 hover:bg-neutral-800">
-                    ${filePath}<span class="pl-2 w-full"></span>
+                    ${fileName}<span class="pl-2 w-full"></span>
                 </li>`;
             selectedFiles.innerHTML += element;
             filesList.push(filePath);
@@ -26,11 +27,12 @@ function getFiles(e) {
 function getFilesFromFolder(e) {
     const files = e.target.files;
     for (let file in files) {
-        const filePath = files[file].webkitRelativePath;
-        if (filePath) {
+        const fileRelativePath = files[file].webkitRelativePath;
+        const filePath = files[file].path;
+        if (fileRelativePath) {
             const element = `
             <li class="list-none w-full px-2 py-px text-start border-b-2 border-neutral-700 hover:bg-neutral-800">
-                ${filePath}<span class="pl-2 w-full"></span>
+                ${fileRelativePath}<span class="pl-2 w-full"></span>
             </li>`;
             selectedFiles.innerHTML += element;
             filesList.push(filePath);
@@ -44,4 +46,4 @@ clearBtn.addEventListener('click', () => {
     filesList = [];
     selectedFiles.innerHTML = '';
     clearBtn.classList.add('hidden');
-})
+});
