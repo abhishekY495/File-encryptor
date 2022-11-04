216d0a04de817122b6351b0e5ea672e915498b6d
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-04 23:58:16 +0530

    Exposed file encryption/decryption

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -1,4 +1,6 @@
-const { contextBridge } = require('electron');
-const { file } = require('../backend/fileEncDec.js');
+const { contextBridge } = require("electron");
+const { encryptFile } = require("../backend/encryptFile.js");
+const { decryptFile } = require("../backend/decryptFile.js");
 
-contextBridge.exposeInMainWorld('file', file);
+contextBridge.exposeInMainWorld("encryptFile", encryptFile);
+contextBridge.exposeInMainWorld("decryptFile", decryptFile);
