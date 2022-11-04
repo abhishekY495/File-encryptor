000a65f8abc4fbb47aff28b592ae3901c58a7eb1
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-04 23:53:46 +0530

    Create encryptFile.js

---
+++ app/backend/encryptFile.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const fs = require("fs");
+const { pipeline } = require("stream/promises");
+const { Transform } = require("stream");
+const { app } = require("./encryption.js");
+
+const encHighWaterMark = 1024 * 1024 * 100;
+
+async function encryptFile(fileLocation, password) {
+  const fileReadStream = fs.createReadStream(fileLocation, {
+    highWaterMark: encHighWaterMark,
+  });
+
+  const filePath = path.parse(fileLocation).dir;
+  const fileName = path.parse(fileLocation).name;
+  const fileExt = path.parse(fileLocation).ext;
+  const newEncFile = filePath + "\\" + fileName + "_ENC" + fileExt;
+  const fileWriteStream = fs.createWriteStream(newEncFile);
+
+  await pipeline(
+    fileReadStream,
+    new Transform({
+      transform(chunk, encoding, callback) {
+        const encryptedData = app.encrypt(chunk, password);
+        callback(null, encryptedData);
+        console.log("File encrypted");
+      },
+    }),
+    fileWriteStream
+  );
+}
+
+module.exports.encryptFile = encryptFile;
+module.exports.encHighWaterMark = encHighWaterMark;
