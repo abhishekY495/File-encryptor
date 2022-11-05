bddd5339a1800ecd64c41b232dc9b0d996b08bc4
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-05 18:07:06 +0530

    Delete noPasswordInput.js

--- app/src/js/errors-JS/noPasswordInput.js
+++
@@ -1,8 +0,0 @@
-const passwordInput = document.querySelector("#password-input");
-
-export function noPasswordInput() {
-  passwordInput.classList.add("shake-password-box");
-  setTimeout(() => {
-    passwordInput.classList.remove("shake-password-box");
-  }, "500");
-}
