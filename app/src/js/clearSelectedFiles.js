6d441269c65e60dc93fb8eef5b6e154f769ba870
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-06 20:01:41 +0530

    Feat: Clear selected files

--- app/src/js/clearSelectedFiles.js
+++ app/src/js/clearSelectedFiles.js
@@ -1,6 +1,11 @@
-const clearBtn = document.querySelector('#clear-btn');
-clearBtn.addEventListener('click', () => {
-    filesList = [];
-    selectedFiles.innerHTML = '';
-    clearBtn.classList.add('hidden');
+import { filesList } from "./browseFile.js";
+const clearBtn = document.querySelector("#clear-btn");
+const selectedFiles = document.querySelector("#selected-files");
+
+clearBtn.addEventListener("click", () => {
+  filesList.length = 0;
+  selectedFiles.innerHTML = "";
+  clearBtn.classList.remove("opacity-100");
+  clearBtn.classList.add("opacity-10");
+  clearBtn.disabled = true;
 });
