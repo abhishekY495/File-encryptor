5139ae508530774bbb6f3c942c8f21c5b9276a1f
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-06 19:59:33 +0530

    Update togglePassword.js

--- app/src/js/togglePassword.js
+++ app/src/js/togglePassword.js
@@ -1,13 +1,13 @@
-const passwordInput = document.querySelector('#password-input');
-const togglePasswordBtn = document.querySelector('#toggle-password');
+const passwordInput = document.querySelector("#password-input");
+const togglePasswordBtn = document.querySelector("#toggle-password");
 
-togglePasswordBtn.addEventListener('click', togglePassword);
+togglePasswordBtn.addEventListener("click", togglePassword);
 function togglePassword() {
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        togglePasswordBtn.innerText = 'Hide';
-    } else {
-        passwordInput.type = 'password';
-        togglePasswordBtn.innerText = 'Show';
-    }
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    togglePasswordBtn.innerText = "Hide";
+  } else {
+    passwordInput.type = "password";
+    togglePasswordBtn.innerText = "Show";
+  }
 }
