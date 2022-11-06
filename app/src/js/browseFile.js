86e0489677a3e012c0ff946401e9970bdbc684e6
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-06 20:01:08 +0530

    Refractor

--- app/src/js/browseFile.js
+++ app/src/js/browseFile.js
@@ -1,49 +1,49 @@
-const browseFilesBtn = document.querySelector('#file-picker');
-const browseFoldersBtn = document.querySelector('#folder-picker');
-const selectedFiles = document.querySelector('#selected-files');
-const clearBtn = document.querySelector('#clear-btn');
+const browseFilesBtn = document.querySelector("#file-picker");
+const browseFoldersBtn = document.querySelector("#folder-picker");
+const selectedFiles = document.querySelector("#selected-files");
+const clearBtn = document.querySelector("#clear-btn");
 export let filesList = [];
 
-browseFilesBtn.addEventListener('change', (e) => getFiles(e));
-browseFoldersBtn.addEventListener('change', (e) => getFilesFromFolder(e));
+browseFilesBtn.addEventListener("change", (e) => getFiles(e));
+browseFoldersBtn.addEventListener("change", (e) => getFilesFromFolder(e));
+
+browseFilesBtn.addEventListener("click", (e) => (e.target.value = null));
+browseFoldersBtn.addEventListener("click", (e) => (e.target.value = null));
 
 function getFiles(e) {
-    const files = e.target.files;
-    for (let file in files) {
-        const fileName = files[file].name;
-        const filePath = files[file].path;
-        if (fileName) {
-            const element = `
-                <li class="list-none w-full px-2 py-px text-start border-b-2 border-neutral-700 hover:bg-neutral-800">
-                    ${fileName}<span class="pl-2 w-full"></span>
-                </li>`;
-            selectedFiles.innerHTML += element;
-            filesList.push(filePath);
-            clearBtn.classList.remove('hidden');
-        } else return;
-    }
+  filesList = [];
+  const files = e.target.files;
+  for (let file in files) {
+    const fileName = files[file].name;
+    const filePath = files[file].path;
+    if (fileName) {
+      const element = `
+        <li class="list-none w-full px-2 py-px text-start border-b-2 border-neutral-700 hover:bg-neutral-800">
+            ${fileName}<span class="pl-2 w-full"></span>
+        </li>`;
+      selectedFiles.innerHTML += element;
+      filesList.push(filePath);
+      clearBtn.classList.remove("opacity-10");
+      clearBtn.disabled = false;
+    } else return;
+  }
 }
 
 function getFilesFromFolder(e) {
-    const files = e.target.files;
-    for (let file in files) {
-        const fileRelativePath = files[file].webkitRelativePath;
-        const filePath = files[file].path;
-        if (fileRelativePath) {
-            const element = `
-            <li class="list-none w-full px-2 py-px text-start border-b-2 border-neutral-700 hover:bg-neutral-800">
-                ${fileRelativePath}<span class="pl-2 w-full"></span>
-            </li>`;
-            selectedFiles.innerHTML += element;
-            filesList.push(filePath);
-            clearBtn.classList.remove('hidden');
-        } else return;
-    }
+  filesList = [];
+  const files = e.target.files;
+  for (let file in files) {
+    const filePathName = files[file].webkitRelativePath;
+    const filePath = files[file].path;
+    if (filePathName) {
+      const element = `
+        <li class="list-none w-full px-2 py-px text-start border-b-2 border-neutral-700 hover:bg-neutral-800">
+            ${filePathName}<span class="pl-2 w-full"></span>
+        </li>`;
+      selectedFiles.innerHTML += element;
+      filesList.push(filePath);
+      clearBtn.classList.remove("opacity-10");
+      clearBtn.disabled = false;
+    } else return;
+  }
 }
-
-// Clear Selected files
-clearBtn.addEventListener('click', () => {
-    filesList = [];
-    selectedFiles.innerHTML = '';
-    clearBtn.classList.add('hidden');
-});
