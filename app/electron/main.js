b6dba64ac5f86fcbe4fecd0a2721bfac36775581
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-07 14:59:45 +0530

    Refractor

--- app/electron/main.js
+++ app/electron/main.js
@@ -1,46 +1,43 @@
-const path = require('path');
-const { app, BrowserWindow, Menu } = require('electron');
-const { menuTemplate } = require('./menuTemplate.js');
+const path = require("path");
+const { app, BrowserWindow, Menu } = require("electron");
+const { menuTemplate } = require("./menuTemplate.js");
 
-// 
-const isMac = process.platform === 'darwin';
-const isDev = process.env.NODE_ENV !== 'development';
+const isMac = process.platform === "darwin";
 
 function createMainWindow() {
-    const mainWindow = new BrowserWindow({
-        title: "File encryptor",
-        width: 420,
-        height: 690,
-        webPreferences: {
-            contextIsolation: true,
-            nodeIntegration: true,
-            preload: path.join(__dirname, './preload.js')
-        }
-    });
+  const mainWindow = new BrowserWindow({
+    title: "File encryptor",
+    width: 420,
+    height: 690,
+    // resizable: false,
+    icon: path.join(__dirname, "./icon.ico"),
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, "./preload.js"),
+    },
+  });
 
-    // Open DevTools
-    if (isDev) {
-        mainWindow.webContents.openDevTools();
-    }
+  mainWindow.webContents.openDevTools();
 
-    mainWindow.loadFile(path.join(__dirname, '../src/index.html'));
+  mainWindow.loadFile(path.join(__dirname, "../src/index.html"));
 }
 
 app.whenReady().then(() => {
-    createMainWindow();
+  createMainWindow();
 
-    const menu = Menu.buildFromTemplate(menuTemplate);
-    Menu.setApplicationMenu(menu);
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createMainWindow();
-        };
-    });
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
 });
 
-app.on('window-all-closed', () => {
-    if (!isMac) {
-        app.quit();
-    };
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
 });
