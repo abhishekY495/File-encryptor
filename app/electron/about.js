ff5a300735d2eef54f4695deb160eda19456fbd6
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-08 18:01:39 +0530

    Updated window height

--- app/electron/about.js
+++ app/electron/about.js
@@ -5,8 +5,9 @@ function createAboutWindow() {
   const aboutWindow = new BrowserWindow({
     title: "About",
     width: 400,
-    height: 200,
+    height: 211,
     autoHideMenuBar: true,
+    resizable: false,
     icon: path.join(__dirname, "./icons/about-icon.ico"),
   });
   aboutWindow.loadFile(path.join(__dirname, "../src/about.html"));
