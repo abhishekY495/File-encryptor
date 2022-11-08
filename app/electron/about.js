b0f207b1ad01f3a9442406a5a88341e10503f62f
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-08 17:49:16 +0530

    Refractor

--- app/electron/about.js
+++ app/electron/about.js
@@ -1,14 +1,15 @@
-const path = require('path');
-const { BrowserWindow } = require('electron');
+const path = require("path");
+const { BrowserWindow } = require("electron");
 
 function createAboutWindow() {
-    const aboutWindow = new BrowserWindow({
-        title: "About",
-        width: 400,
-        height: 200,
-        autoHideMenuBar: true
-    });
-    aboutWindow.loadFile(path.join(__dirname, '../src/about.html'));
+  const aboutWindow = new BrowserWindow({
+    title: "About",
+    width: 400,
+    height: 200,
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, "./icons/about-icon.ico"),
+  });
+  aboutWindow.loadFile(path.join(__dirname, "../src/about.html"));
 }
 
 module.exports.createAboutWindow = createAboutWindow;
--- app/electron/menuTemplate.js
+++ app/electron/menuTemplate.js
@@ -1,25 +1,26 @@
-const { createAboutWindow } = require('./about.js');
-const { app, shell } = require('electron');
+const { createAboutWindow } = require("./about.js");
+const { app, shell } = require("electron");
 
 const menuTemplate = [
-    {
-        label: 'Help',
-        submenu: [
-            {
-                label: 'About',
-                click: createAboutWindow
-            },
-            {
-                label: 'Project code - Github',
-                click: () => shell.openExternal('https://github.com/abhishake21/File-encryptor')
-            },
-            {
-                label: 'Quit',
-                click: () => app.quit(),
-                accelerator: `CmdOrCtrl+W`
-            }
-        ]
-    }
+  {
+    label: "Help",
+    submenu: [
+      {
+        label: "About",
+        click: createAboutWindow,
+      },
+      {
+        label: "Project code - Github",
+        click: () =>
+          shell.openExternal("https://github.com/abhishake21/File-encryptor"),
+      },
+      {
+        label: "Quit",
+        click: () => app.quit(),
+        accelerator: `CmdOrCtrl+W`,
+      },
+    ],
+  },
 ];
 
 module.exports.menuTemplate = menuTemplate;
