2fe1106bc8720a9c05b93386a87796dc1584e559
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-09 20:24:18 +0530

    Added unlinkSync to delete files

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -1,6 +1,11 @@
 const { contextBridge } = require("electron");
 const { encryptFile } = require("../backend/encryptFile.js");
 const { decryptFile } = require("../backend/decryptFile.js");
+const fs = require("fs");
 
 contextBridge.exposeInMainWorld("encryptFile", encryptFile);
 contextBridge.exposeInMainWorld("decryptFile", decryptFile);
+
+contextBridge.exposeInMainWorld("unlinkSync", {
+  unlinkSync: (path) => fs.unlinkSync(path),
+});
