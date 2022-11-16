89a4062fff87a2b443dd026decda486f5b83f4cd
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-16 16:06:00 +0530

    Added Notification and Un/Scramble options

--- app/electron/menuTemplate.js
+++ app/electron/menuTemplate.js
@@ -1,6 +1,10 @@
-const { createAboutWindow } = require("./about.js");
 const { app, shell } = require("electron");
+const { createAboutWindow } = require("./about.js");
+const { desktopNotificationSettings, saveDesktopNotificationSettings } = require("./notificationSettings.js");
+const { scrambleFileNameSettings, saveScrambleFileNameSetting } = require("./scrambleFileName.js");
 
+let notification = desktopNotificationSettings();
+let scramble = scrambleFileNameSettings();
 const menuTemplate = [
   {
     label: "Help",
@@ -11,8 +15,19 @@ const menuTemplate = [
       },
       {
         label: "Project code - Github",
-        click: () =>
-          shell.openExternal("https://github.com/abhishake21/File-encryptor"),
+        click: () => shell.openExternal("https://github.com/abhishake21/File-encryptor"),
+      },
+      {
+        label: "Desktop Notifications",
+        type: "checkbox",
+        checked: notification,
+        click: () => saveDesktopNotificationSettings((notification = !notification)),
+      },
+      {
+        label: "Un/Scramble File names",
+        type: "checkbox",
+        checked: scramble,
+        click: () => saveScrambleFileNameSetting((scramble = !scramble)),
       },
       {
         label: "Quit",
@@ -21,6 +36,12 @@ const menuTemplate = [
       },
     ],
   },
+  {
+    label: "Reload",
+    role: "reload"
+  }
 ];
 
 module.exports.menuTemplate = menuTemplate;
+module.exports.notification = notification;
+module.exports.scramble = scramble;
