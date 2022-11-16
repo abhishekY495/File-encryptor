01b8d23b52cde6904c0dccda64b4b178aee5cee2
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-11-16 15:38:54 +0530

    Exposing sendNotification to renderer

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -1,10 +1,12 @@
 const { contextBridge } = require("electron");
 const { encryptFile } = require("../backend/encryptFile.js");
 const { decryptFile } = require("../backend/decryptFile.js");
+const { sendNotification } = require("../backend/sendNotification.js");
 const fs = require("fs");
 
 contextBridge.exposeInMainWorld("encryptFile", encryptFile);
 contextBridge.exposeInMainWorld("decryptFile", decryptFile);
+contextBridge.exposeInMainWorld("sendNotification", sendNotification);
 
 contextBridge.exposeInMainWorld("unlinkSync", {
   unlinkSync: (path) => fs.unlinkSync(path),
